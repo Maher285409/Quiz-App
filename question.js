2d7a2ff3d3072aa4475cd4b2a97cf3feffacc246
MAHER <maherkainaf24@gmail.com>
2025-11-10 17:10:57 +0530

    quiz app

---
+++ question.js
@@ -0,0 +1,73 @@
+// creating an array and passing the Number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: 'Which language is used to structure the content of a webpage?',
+    answer: 'Markup',
+    options: ['Programming', 'Scripting', 'Markup', 'Styling'],
+  },
+  {
+    numb: 2,
+    question: 'Which CSS property is used to change the text color of an element?',
+    answer: 'color',
+    options: ['font-style', 'color', 'background-color', 'text-align'],
+  },
+  {
+    numb: 3,
+    question: 'Which tag is used to insert a line break in HTML?',
+    answer: '<br>',
+    options: ['<break>', '<br>', '<lb>', '<line>'],
+  },
+  {
+    numb: 4,
+    question: 'Which of the following is NOT a valid CSS unit?',
+    answer: 'sec',
+    options: ['px', 'em', 'cm', 'sec'],
+  },
+  {
+    numb: 5,
+    question: 'In JavaScript, which symbol is used for single-line comments?',
+    answer: '//',
+    options: ['<!-- -->', '//', '/* */', '##'],
+  },
+  {
+    numb: 6,
+    question: 'What does CSS stand for?',
+    answer: 'Cascading Style Sheets',
+    options: [
+      'Colorful Style Sheets',
+      'Computer Style Sheets',
+      'Cascading Style Sheets',
+      'Creative Styling System',
+    ],
+  },
+  {
+    numb: 7,
+    question: 'Which of the following is used to make a website responsive?',
+    answer: 'Flexbox',
+    options: ['Tables', 'Flexbox', 'Inline CSS', 'Fixed layout'],
+  },
+  {
+    numb: 8,
+    question: 'What is the correct HTML tag for inserting an image?',
+    answer: '<img>',
+    options: ['<img>', '<image>', '<picture>', '<src>'],
+  },
+  {
+    numb: 9,
+    question: 'Which JavaScript method is used to print something in the browser console?',
+    answer: 'console.log()',
+    options: ['print()', 'console.write()', 'console.log()', 'document.write()'],
+  },
+  {
+    numb: 10,
+    question: 'What is the full form of API?',
+    answer: 'Application Programming Interface',
+    options: [
+      'Application Programming Interface',
+      'Advanced Programming Integration',
+      'Application Process Interface',
+      'Applied Programming Interaction',
+    ],
+  },
+];
---
+++ script.js
@@ -0,0 +1,205 @@
+let startBtn = document.querySelector(".startBtn");
+let infoBox = document.querySelector(".infoBox");
+let exitBtn = document.querySelector(".exitBtn");
+let continueBtn = document.querySelector(".continueBtn");
+let quizBox = document.querySelector(".quiz-box");
+let questionText = document.querySelector(".questionText");
+let allOptions = document.querySelectorAll(".options");
+let nextBtn = document.querySelector(".nextBtn");
+let timeline = document.querySelector(".timeline");
+let currentQuestionIndicator = document.querySelector(
+  ".currentQuestionIndicator"
+);
+let progressBar = document.querySelector(".progressBar");
+let timeLineTitle = document.querySelector(".time-line-title");
+
+let replayQuiz = document.querySelector(".replay_Quiz");
+let quitQuiz = document.querySelector(".quit_Quiz");
+let resultBox = document.querySelector(".result-box");
+let scoreText = document.querySelector(".score_text");
+
+let userScore = 0;
+let currentQuestionIndex = 0;
+
+let timeLineInterval = null;
+let progressBarInterval = null;
+const TickIcon = `<div class="icon tick">
+    <i class="fa-solid fa-check"></i>
+  </div>`;
+
+const CrossIcon = `<div class="icon cross">
+    <i class="fa-solid fa-xmark"></i>
+  </div>`;
+console.log(startBtn);
+
+startBtn.addEventListener("click", () => {
+  // we have to inject a class name to info box
+  infoBox.classList.add("activeInfoBox");
+});
+
+exitBtn.addEventListener("click", () => {
+  infoBox.classList.remove("activeInfoBox");
+});
+
+continueBtn.addEventListener("click", () => {
+  infoBox.classList.remove("activeInfoBox");
+  quizBox.classList.add("activeQuizBox");
+  // here we need to start showing questions
+  showQuestion(currentQuestionIndex);
+  handleTiming(15);
+  handleProgressBar();
+  timeLineTitle.innerText = 'Time Left';
+});
+
+nextBtn.addEventListener("click", () => {
+  if (currentQuestionIndex < 9) {
+    currentQuestionIndex = currentQuestionIndex + 1;
+    // reset the time
+    // reset the progress bar
+    handleTiming(15);
+    handleProgressBar();
+    showQuestion(currentQuestionIndex);
+    nextBtn.classList.remove("active");
+    timeLineTitle.innerText = 'Time Left';
+  }
+  else {
+    //you reached the last question
+    //you need to show result
+    clearInterval(progressBarInterval);
+    clearInterval(timeLineInterval);
+    quizBox.classList.remove('activeQuizBox');
+    resultBox.classList.add('activeResultBox');
+    handleShowResults();
+  }
+});
+
+quitQuiz.addEventListener('click', ()=>{
+  restart();
+  resultBox.classList.remove('activeResultBox');
+});
+
+replayQuiz.addEventListener('click', ()=> {
+  restart();
+  resultBox.classList.remove('activeResultBox');
+  quizBox.classList.add("activeQuizBox");
+  showQuestion(currentQuestionIndex);
+  handleTiming(15);
+  handleProgressBar();
+  timeLineTitle.innerText = 'Time Left';
+});
+
+// function to show/render questions
+const showQuestion = (index) => {
+  // console.log(questions);
+  questionText.innerText =
+    "" + questions?.[index].numb + ". " + questions?.[index].question;
+
+  for (let i = 0; i < allOptions?.length; i++) {
+    allOptions[i].innerText = questions?.[index].options?.[i];
+    allOptions[i].classList.remove("correct");
+    allOptions[i].classList.remove("incorrect");
+    allOptions[i].classList.remove("disabled");
+
+    if (index === 0) {
+      allOptions[i]?.addEventListener("click", optionClickHandler);
+    }
+  }
+
+  currentQuestionIndicator.innerText = index + 1;
+};
+
+const handleTiming = (time) => {
+  timeline.innerText = time;
+  let timeValue = time;
+  timeLineInterval = setInterval(() => {
+    timeValue--;
+
+    if (timeValue < 10) {
+      timeline.innerText = "0" + timeValue;
+    } else {
+      timeline.innerText = timeValue;
+    }
+
+    if (timeValue === 0) {
+      // you cross the time
+      timeLineTitle.innerText = 'Time Off';
+      clearInterval(timeLineInterval);
+      nextBtn.classList.add("active");
+      const correctAnswer = questions[currentQuestionIndex].answer;
+      for (let i = 0; i < allOptions?.length; i++) {
+        allOptions[i].classList.add("disabled");
+
+        if (
+          
+          allOptions[i].innerText === correctAnswer
+        ) {
+          allOptions[i].classList.add("correct");
+          allOptions[i].insertAdjacentHTML("beforeend", TickIcon);
+        }
+      } //display correct answer
+    }
+  }, 1000);
+};
+
+const handleProgressBar = () => {
+  progressBar.style.width = "0%";
+  let currentPercentage = 0;
+  progressBarInterval = setInterval(() => {
+    currentPercentage += 100 / 1500;
+    progressBar.style.width = currentPercentage + "%";
+
+    if (currentPercentage >= 100) {
+      clearInterval(progressBarInterval);
+    }
+  }, 10);
+};
+
+
+
+const optionClickHandler = (e) => {
+  clearInterval(progressBarInterval);
+  clearInterval(timeLineInterval);
+  nextBtn.classList.add("active");
+  const userAnswer = e.target.innerText;
+  const correctAnswer = questions[currentQuestionIndex].answer;
+
+  if (userAnswer === correctAnswer) {
+    userScore++;
+    e.target.classList.add("correct");
+    e.target.insertAdjacentHTML("beforeend", TickIcon);
+  } else {
+    e.target.classList.add("incorrect");
+    e.target.insertAdjacentHTML("beforeend", CrossIcon);
+  }
+
+  for (let i = 0; i < allOptions?.length; i++) {
+    allOptions[i].classList.add("disabled");
+
+    if (
+      userAnswer !== correctAnswer &&
+      allOptions[i].innerText === correctAnswer
+    ) {
+      allOptions[i].classList.add("correct");
+      allOptions[i].insertAdjacentHTML("beforeend", TickIcon);
+    }
+  }
+};
+
+const restart =()=> {
+  clearInterval(progressBarInterval);
+  clearInterval(timeLineInterval);
+  userScore = 0;
+  currentQuestionIndex = 0;
+  timeLineTitle.innerText = 'Time Left';
+}
+
+const handleShowResults=()=>{
+
+  scoreText.innerHTML=
+        `<span
+          >and nice 😎, You got
+          <p>${userScore}</p>
+          out of
+          <p>${questions?.length}</p></span
+        >`;
+};
